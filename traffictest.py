d673f417fdd77c57c7f5accfdf63fae09a74151c
Patrick Emery <patrickemery@mail.usf.edu>
2016-10-27 11:02:56 -0400

    Update

--- traffictest.py
+++ traffictest.py
@@ -1,7 +1,3 @@
-import plotly.plotly as py
-import plotly.tools as tls
-tls.set_credentials_file(username='patemeryfl', api_key='09kwa5ad0c')
-import plotly.graph_objs as go
 import urllib2
 import re
 import xml.etree.ElementTree as ET
@@ -15,17 +11,19 @@ localFile.close()
 #Parse the xml file and define the root of the xml
 tree = ET.parse('newtrafficdata.xml')
 root = tree.getroot()
-#Put new data into array
+#Put new data into array, define a master list to merge new data with
 newTraffic=[]
+masterList=[]
 #For loop to seach through xml for 'item' then get the time and address, and append to traffic array
 for channel in root.iter('item'):
     time = channel.find('pubDate').text
     address = channel.find('description').text
+    #time = re.split() regex code to filter 'address and data after that' from description
     newTraffic.append([time, address])
-    go.Heatmap() #Send data to heatmap for plotly
 #print(newtraffic[0].find("address", "<"))
+
+#verbose output of data
 print(newTraffic)
-#Append deletes previous data, so you can merge the new data with the maste rlist
+#Append deletes previous data, so you can merge the new data with the master list
 masterList = masterList + newTraffic
-#Sends new data to plotly
-py.iplot(data, filename='traffic-heatmap')
+#print(masterList)
